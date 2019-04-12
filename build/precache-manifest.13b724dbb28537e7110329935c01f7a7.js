15f4906dd7ed85f0b7b093c87fd27974aaf9188c
Andy <andy.egyud@gmail.com>
2019-04-12 10:16:18 -0700

    Heroku commit

---
+++ build/precache-manifest.13b724dbb28537e7110329935c01f7a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eceb22ab76a5a141c3b2",
+    "url": "/static/css/main.439b2f64.chunk.css"
+  },
+  {
+    "revision": "eceb22ab76a5a141c3b2",
+    "url": "/static/js/main.603dff3c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "604382ed4135712340b8",
+    "url": "/static/js/2.b9a33529.chunk.js"
+  },
+  {
+    "revision": "81d82b1f199748e9675382a564d84d33",
+    "url": "/static/media/blue-sky-mobile.81d82b1f.jpeg"
+  },
+  {
+    "revision": "1a6abfff7f4ecf11b3296afc8c627af4",
+    "url": "/static/media/blue-sky.1a6abfff.jpg"
+  },
+  {
+    "revision": "7a9e9a1e2a73aed89f223a4f94dbc35b",
+    "url": "/index.html"
+  }
+];
